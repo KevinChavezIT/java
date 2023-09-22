3c8a73767e969da1e845bb92ef66ad6e1ea9b371
KevinChavezIT <hc7657819@gmail.com>
2023-09-22 17:39:54 -0500

    metodos funcionando correctamente

--- eclipse-workspace/Clases/src/com_krakedev/Calculadora.java
+++ eclipse-workspace/Clases/src/com_krakedev/Calculadora.java
@@ -17,4 +17,33 @@ public class Calculadora {
 
 		return respuesta;
 	}
+	
+	public double multiplicar(double num1, double num2) {
+
+		double respuesta;
+		respuesta = num1 * num2;
+
+		return respuesta;
+	}
+	
+	public double dividir(double num1, double num2) {
+
+		double respuesta;
+		respuesta = num1 / num2;
+
+		return respuesta;
+	}
+	
+	public double promediar(double num1, double num2,double num3) {
+
+		double respuesta;
+		respuesta = (num1 + num2+num3)/3;
+
+		return respuesta;
+	}
+	
+	public void mostrarResultado() {
+
+		System.out.println("Ahorita no joven, Sali al almuerzo. Regreos en 15 minutos");
+	}
 }
--- eclipse-workspace/Clases/src/com_krakedev/TesteCalculadora.java
+++ eclipse-workspace/Clases/src/com_krakedev/TesteCalculadora.java
@@ -6,15 +6,32 @@ public class TesteCalculadora {
 
 		int respuestaSuma;
 		int respuestaResta;
+		double respuestaMulti;
+		double respuestaDivision;
+		double respuestaPromedio;
 
 		Calculadora suma = new Calculadora();
 		Calculadora resta = new Calculadora();
+		Calculadora multiplicacion = new Calculadora();
+		Calculadora division = new Calculadora();
+		Calculadora promedio = new Calculadora();
+		Calculadora mensaje = new Calculadora();
 
-		respuestaSuma = suma.sumar(5, 10);
+		respuestaSuma = suma.sumar(5, 5);
 
-		respuestaResta = resta.restar(10, 5);
+		respuestaResta = resta.restar(20, 5);
+		
+		respuestaMulti = multiplicacion.multiplicar(5, 4);
+
+		respuestaDivision = division.dividir(100, 4);
+		
+		respuestaPromedio = promedio.promediar(10, 10,10);
 
 		System.out.println("Respuesta SUMA: " + respuestaSuma);
 		System.out.println("Respuesta RESTA: " + respuestaResta);
+		System.out.println("Respuesta MULTIPLIACION: " + respuestaMulti);
+		System.out.println("Respuesta DIVISION: " + respuestaDivision);
+		System.out.println("Respuesta PROMEDIO: " + respuestaPromedio);
+		mensaje.mostrarResultado();
 	}
 }
