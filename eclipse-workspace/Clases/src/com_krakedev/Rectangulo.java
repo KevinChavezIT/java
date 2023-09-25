110d37da88731a21e71d3997dee5d4ff117b5d73
KevinChavezIT <hc7657819@gmail.com>
2023-09-24 21:48:59 -0500

    clase Rectangulo encapsulada

--- eclipse-workspace/Clases/src/com_krakedev/Rectangulo.java
+++ eclipse-workspace/Clases/src/com_krakedev/Rectangulo.java
@@ -2,21 +2,41 @@ package com_krakedev;
 
 public class Rectangulo {
 
-	public int base;
-	public int altura;
+	private int base;
+	private int altura;
+	private int calcularArea;
+	private double calcularPerimetro;
 
-	public int calcularArea() {
-
-		int area;
-		area = base * altura;
-		return area;
+	public int getBase() {
+		return base;
 	}
 
-	public double calcularPerimetro() {
+	public void setBase(int base) {
+		this.base = base;
+	}
 
-		double perimetro;
-		perimetro = base * 2 + altura * 2;
-		return perimetro;
+	public int getAltura() {
+		return altura;
+	}
+
+	public void setAltura(int altura) {
+		this.altura = altura;
+	}
+
+	public int getCalcularArea() {
+		return calcularArea;
+	}
+
+	public void setCalcularArea() {
+		calcularArea = base * altura;
+	}
+
+	public double getCalcularPerimetro() {
+		return calcularPerimetro;
+	}
+
+	public void setCalcularPerimetro() {
+		calcularPerimetro = base * 2 + altura * 2;
 	}
 
 }
--- eclipse-workspace/Clases/src/com_krakedev/TestRectanguloo.java
+++ eclipse-workspace/Clases/src/com_krakedev/TestRectanguloo.java
@@ -3,32 +3,35 @@ package com_krakedev;
 public class TestRectanguloo {
 
 	public static void main(String[] args) {
-		
-		Rectangulo a= new Rectangulo();
-		Rectangulo a2= new Rectangulo();
-		
+
+		Rectangulo a = new Rectangulo();
+		Rectangulo a2 = new Rectangulo();
+
 		int area;
 		int area2;
 		double perimetro;
 		double perimetro2;
-		
-		a.altura=10;
-		a.base=5;
-		
-		a2.base=8;
-		a2.altura=3;
-		
-		area= a.calcularArea();
-		System.out.println("Area1 igual: "+area);
-		perimetro=a.calcularPerimetro();
-		System.out.println("Perimetro igual: "+perimetro);
+
+		a.setAltura(10);
+		a.setBase(5);
+		a2.setAltura(8);
+		a2.setBase(3);
+
+		a.setCalcularArea();
+		a.setCalcularPerimetro();
+		area = a.getCalcularArea();
+		System.out.println("Area1 igual: " + area);
+		perimetro = a.getCalcularPerimetro();
+		System.out.println("Perimetro igual: " + perimetro);
 		
 		System.out.println("------------------------------");
 		
-		area2=a2.calcularArea();
-		System.out.println("Area igual: "+area2);
-		perimetro2=a2.calcularPerimetro();
-		System.out.println("Perimetro igual: "+perimetro2);
+		a2.setCalcularArea();
+		a2.setCalcularPerimetro();
+		area2 = a2.getCalcularArea();
+		System.out.println("Area igual: " + area2);
+		perimetro2 = a2.getCalcularPerimetro();
+		System.out.println("Perimetro igual: " + perimetro2);
 	}
 
 }
