3baa2c2dac4d46795fb8054d6408cdda63c5954f
KevinChavezIT <hc7657819@gmail.com>
2023-09-24 22:55:50 -0500

    clase Fecha, constructor con parametros agregado

--- eclipse-workspace/Clases/src/com_krakedev/TestFecha.java
+++ eclipse-workspace/Clases/src/com_krakedev/TestFecha.java
@@ -4,12 +4,8 @@ public class TestFecha {
 
 	public static void main(String[] args) {
 		
-		Fecha fecha1=new Fecha();
-		
-		fecha1.setAnio(20000);
-		fecha1.setMes(6);
-		fecha1.setDia(7);
-		
+		Fecha fecha1=new Fecha(2000,6,7);
+
 		System.out.println("Fecha: "+fecha1.getAnio()
 		+"-"+fecha1.getMes()+"-"+fecha1.getDia());
 	}
