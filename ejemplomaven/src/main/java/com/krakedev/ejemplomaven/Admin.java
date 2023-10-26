c6bb0342a7d33f14a4267b19dbba491545ea8b7e
KevinChavezIT <hc7657819@gmail.com>
2023-10-26 13:38:36 -0500

    video

---
+++ ejemplomaven/src/main/java/com/krakedev/ejemplomaven/Admin.java
@@ -0,0 +1,19 @@
+package com.krakedev.ejemplomaven;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Admin {
+
+	private static Logger logger = LogManager.getLogger(Admin.class);
+	
+	public void agregar() {
+		logger.warn("MENSAJE DE WARN"); 
+		logger.info("MENSAJE DE INFO"); 
+		logger.debug("MENSAJE DE DEBUG"); 
+		logger.trace("MENSAJE DE TRACE"); 
+		logger.error("MENSAJE DE ERROR"); 
+		
+	}
+
+}
---
+++ ejemplounit/src/main/java/com/krakedev/ejemplounit/Calculadora.java
@@ -0,0 +1,18 @@
+package com.krakedev.ejemplounit;
+
+public class Calculadora {
+
+	public int sumar(int num1, int num2) {
+
+		int respuesta = num1 + num2;
+
+		return respuesta;
+	}
+
+	public int restar(int num1, int num2) {
+
+		int respuesta = num1 - num2;
+
+		return respuesta;
+	}
+}
